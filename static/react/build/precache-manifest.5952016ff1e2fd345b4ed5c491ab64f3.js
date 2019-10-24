80c0da677d16180ecdf10bdd70f8e869b6d17ddc
Serq108 <49933099+Serq108@users.noreply.github.com>
2019-10-24 07:16:31 +0300

    Add files via upload

---
+++ static/react/build/precache-manifest.5952016ff1e2fd345b4ed5c491ab64f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d440a340aa018778c3f7",
+    "url": "/static/js/main.d2e511ab.chunk.js"
+  },
+  {
+    "revision": "c50c849fa7ee9eac6079",
+    "url": "/static/js/2.c710e804.chunk.js"
+  },
+  {
+    "revision": "d440a340aa018778c3f7",
+    "url": "/static/css/main.62ab2534.chunk.css"
+  },
+  {
+    "revision": "c50c849fa7ee9eac6079",
+    "url": "/static/css/2.87694471.chunk.css"
+  },
+  {
+    "revision": "1806890f8dcd87472b44f2e66a6a12ca",
+    "url": "/index.html"
+  }
+];
